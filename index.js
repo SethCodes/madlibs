12243f79a155af4bde03263803ef16022c40e55c
Seth Stephens <sethastephens8@gmail.com>
2021-07-21 09:57:26 -0400

    added opacity to answer card

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const madLib = document.getElementById("answer");
 
 
 
+
 const lib = () => {
+    let answerCard = document.getElementById("answerCard");
+
+    answerCard.classList.remove("invis");
+    answerCard.classList.add("vis");
+console.log(answerCard.classList);
+
+
 const foodItem = document.getElementById("food-item").value;
 const nounOne = document.getElementById("noun-one").value;
 const verb = document.getElementById("verb").value;
@@ -15,15 +23,41 @@ const nounThree = document.getElementById("noun-three").value;
 const animal = document.getElementById("animal").value;
 const madLib = document.getElementById("answer");
 
-var str = "At night I dream of " + foodItem + " that is as big as a " + nounOne + ". When I try to take a bite, it " + verb + "'s on my " + nounTwo + ". The best time I ever ate " + foodItem + " was in " + place + ". It tasted " + adjOne + ". But don't ever buy it in" + place + " though, it is most " + adjTwo + ". My " + person + " likes it best with " + nounThree + "s. Be sure to give the leftovers to your " + animal + ".";
 
 
-const libArr = [foodItem, nounOne, verb, nounTwo, place,adjOne, adjTwo, person, nounThree, animal];
-madLib.innerHTML = str;
+var str = "At night I dream of " + foodItem + " that is as big as a " + nounOne + ". When I try to take a bite, it " + verb + "'s on my " + nounTwo + ". The best time I ever ate " + foodItem + " was in " + place + ". It tasted " + adjOne + ". But don't ever buy it in " + place + " though, it is most " + adjTwo + ". My " + person + " likes it best with " + nounThree + "s. Be sure to give the leftovers to your " + animal + ".";
 
-libArr.forEach(function(element) {
-    console.log(element);
-});
+
+
+//render string
+madLib.innerHTML= str;
+
+
+
+//remove opacity once string is rendered
+if(madLib.innerHTML != ''){
+
+//clear entries on submit
+foodItem = "";
+nounOne = "";
+verb = "";
+nounTwo = "";
+place = "";
+adjOne = "";
+adjTwo = "";
+person = "";
+nounThree = "";
+animal = "";
+}
+
+
+
+// const libArr = [foodItem, nounOne, verb, nounTwo, place,adjOne, adjTwo, person, nounThree, animal];
+// madLib.innerHTML = str;
+
+// libArr.forEach(function(element) {
+//     console.log(element);
+// });
 
 
 }
